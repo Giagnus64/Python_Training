20f12eb3b9744b8af12f3328ad98d3771793b6a1
Gianfranco <gianfranco.nuschese@channable.com>
2024-10-03 09:48:43 -0400

    Added first section 11 files

---
+++ Section_11_files/app.py
@@ -0,0 +1,57 @@
+from collections import defaultdict, OrderedDict, namedtuple, deque
+from datetime import datetime
+
+
+def task1() -> defaultdict:
+    """
+    - create a `defaultdict` object, and its default value would be set to the string `Unknown`.
+    - Add an entry with key name `Alan` and its value being `Manchester`.
+    - Return the `defaultdict` object you created.
+    """
+    # you code starts here:
+    new_dict = defaultdict(lambda: "Unknown")
+    new_dict["Alan"] = "Manchester"
+    return new_dict
+
+
+def task2(arg_od: OrderedDict):
+    """
+    - takes in an OrderedDict `arg_od`
+    - Remove the first and last entry in `arg_od`.
+    - Move the entry with key name `Bob` to the end of `arg_od`.
+    - Move the entry with key name `Dan` to the start of `arg_od`.
+    - You may assume that `arg_od` would always contain the keys `Bob` and `Dan`,
+        and they won't be the first or last entry initially.
+    """
+    # you code starts here:
+    last_entry = arg_od.popitem()
+    arg_od.move_to_end('Bob')
+    arg_od.move_to_end('Dan', last=False)
+    return arg_od
+
+
+def task3(name: str, club: str) -> namedtuple:
+    """
+    - create a `namedtuple` with type `Player`, and it will have two fields, `name` and `club`.
+    - create a `Player` `namedtuple` instance that has the `name` and `club` field set by the given arguments.
+    - return the `Player` `namedtuple` instance you created.
+    """
+    # you code starts here:
+    pass
+
+
+def task4(arg_deque: deque):
+    """
+    - Manipulate the `arg_deque` in any order you preferred to achieve the following effect:
+        -- remove last element in `deque`
+        -- move the fist (left most) element to the end (right most)
+        -- add an element `Zack`, a string, to the start (left)
+    """
+    # you code starts here:
+    pass
+
+odict = OrderedDict([('a', 1), ('Bob', '2'), ('Dan', 3), ('b', 2), ('c', 3)])
+print(task1())
+print(task2(odict))
+
+print(datetime.now())
